676b987eba5df58378f36bcac59c29f1814b6f50
种下星星の日 <34804413+Dd-ouni@users.noreply.github.com>
2023-05-15 13:53:30 +0800

    add docs

---
+++ site/docs/.vitepress/config.js
@@ -0,0 +1,49 @@
+export default {
+    themeConfig: {
+        siteTitle: "vitepress",
+        nav: [
+            { text: "指南", link: "/guide/installation" },
+            { text: "组件", link: "/components/button/" },
+        ],
+        socialLinks: [
+            { icon: "github", link: "https://github.com/Dd-ouni/dview" },
+        ],
+    },
+    sidebar: {
+        "/guide/": [
+            {
+                text: "基础",
+                items: [
+                    {
+                        text: "安装",
+                        link: "/guide/installation",
+                    },
+                    {
+                        text: "快速开始",
+                        link: "/guide/quickstart",
+                    },
+                ],
+            },
+            {
+                text: "进阶",
+                items: [
+                    {
+                        text: "xx",
+                        link: "/xx",
+                    },
+                ],
+            },
+        ],
+        "/components/": [
+            {
+                text: "基础组件",
+                items: [
+                    {
+                        text: "Button",
+                        link: "/components/button",
+                    }
+                ],
+            }
+        ]
+    },
+};
---
+++ site/docs/components/button/index.md
@@ -0,0 +1,14 @@
+## Button 按钮
+
+<d-button>默认按钮</d-button>
+
+<d-button type="primary">默认按钮</d-button>
+
+::: details 显示代码
+
+```html
+<d-button>默认按钮</d-button>
+<d-button type="primary">默认按钮</d-button>
+```
+
+:::
